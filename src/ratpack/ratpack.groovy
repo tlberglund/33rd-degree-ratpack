6a243d79916e679dd4978d8fd4c7fdf0689c8408
Tim Berglund <tlberglund@github.com>
2013-03-14 11:01:33 +0100

    Working demo

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,3 +1,17 @@
 get('/') {
    render('index.html')
 }
+
+
+get('/lookup') {
+	render('artist.html')
+}
+
+post('/lookup') { request, response ->
+	def url = new URL("http://musicbrainz.org/ws/2/artist?query=${request.form.artist[0]}")
+	def metadata = new XmlSlurper().parseText(url.text)
+	def artistList = metadata.'artist-list'.artist.name
+
+	render('artist.html', artists: artistList)
+}
+
